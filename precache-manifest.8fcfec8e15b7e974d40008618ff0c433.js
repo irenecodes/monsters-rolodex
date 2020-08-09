bc58fc1554a409d85071a93d51763fcc00d6b52c
Irene Truong <irenetruong@Irenes-MacBook-Pro.local>
2020-08-09 03:43:02 -0400

    Updates

---
+++ precache-manifest.8fcfec8e15b7e974d40008618ff0c433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91aa3643de5dbd6b03dc5e425c981453",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "763a160a035b9a3b3aa0",
+    "url": "/monsters-rolodex/static/css/main.8cd486b9.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "763a160a035b9a3b3aa0",
+    "url": "/monsters-rolodex/static/js/main.3def0eca.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
